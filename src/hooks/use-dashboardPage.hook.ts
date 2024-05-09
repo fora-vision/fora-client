2c99f0c6dec5e6f0f97a3491ea447b49502083f6
NikitaShevchik <n.shevchik@softteco.com>
2024-05-09 11:55:28 +0300

    feat(level): add level to user dashboard

--- src/hooks/use-dashboardPage.hook.ts
+++ src/hooks/use-dashboardPage.hook.ts
@@ -3,15 +3,27 @@ import { getUserCourses } from '../utils/API/courses/api-courses';
 import { useToggle } from './use-toggle.hook';
 import { sessionStore } from '../store/sessionStore';
 import { userStore } from '../store/profileStore';
+import { IExpandedCourse } from '../interfaces/ICourse';
 
 export const useDashboardPage = () => {
+  const [level, setLevel] = useState(0)
   const [courses, setCourses] = useState([])
   const [coursesLoading, setCoursesLoading] = useState(true);
   const user = userStore.getUserProfile()
   const [isJoin, toggleJoin] = useToggle();
   const [courseCode, setCourseCode] = useState('');
   const username = user.name;
-  const level = 0; // api data = будет позже 
+
+  useEffect(() => {
+    let totalWorkouts = 0;
+    let totalDoneWorkouts = 0;
+    courses.forEach((course: IExpandedCourse) => {
+      totalDoneWorkouts += course.workout_num
+      totalWorkouts += course.workouts_count
+    })
+    const totalLevel = ((totalWorkouts / totalDoneWorkouts) * 10).toFixed(0)
+    setLevel(Number(totalLevel))
+  }, [courses])
 
   useEffect(() => {
     const fetchCourses = async () => {
